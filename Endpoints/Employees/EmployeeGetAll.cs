e2dfaed919139a35f24b4e89b9d87016d9db2f46
Rafael Pereira <rafahenriquee13@yahoo.com.br>
2022-05-30 20:15:55 -0300

    Creating EmployeeGet and EmployeeResponse

---
+++ Endpoints/Employees/EmployeeGetAll.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShoppingApp.Endpoints.Employees;
+
+public class EmployeeGetAll
+{
+    public static string Template => "/employees";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    {
+        var users = userManager.Users.Skip((page -1) * rows).Take(rows).ToList();
+        var employees = new List<EmployeeResponse>();
+        foreach (var user in users)
+        {
+            var claims = userManager.GetClaimsAsync(user).Result;
+            var claimName = claims.FirstOrDefault(c => c.Type == "Name");
+            var userName = claimName != null ? claimName.Value : string.Empty;
+            employees.Add(new EmployeeResponse(user.Email, userName));
+        }
+        
+        return Results.Ok(employees);
+    }
+}
---
+++ Endpoints/Employees/EmployeeResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace ShoppingApp.Endpoints.Employees;
+
+public record EmployeeResponse(string Email, string Name);
+
