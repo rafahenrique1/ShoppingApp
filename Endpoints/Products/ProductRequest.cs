df219ee7c9bbbdb83a5764abcae5dc944e613e6d
Rafael Pereira <rafahenriquee13@yahoo.com.br>
2022-06-15 13:36:04 -0300

    Add Price field and PriceMigration

--- Endpoints/Products/ProductRequest.cs
+++ Endpoints/Products/ProductRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace ShoppingApp.Endpoints.Products;
 
-public record ProductRequest(string Name, Guid CategoryId, string Description, bool HasStock, bool Active);
+public record ProductRequest(string Name, Guid CategoryId, string Description, bool HasStock, decimal Price, bool Active);
