17065fd3c7a0f5c01c834b413f32d0a1d65a2f56
Rafael Pereira <rafahenriquee13@yahoo.com.br>
2022-06-20 14:04:53 -0300

    Add Order class and Order Migration

---
+++ Domain/Orders/Order.cs
@@ -0,0 +1,41 @@
+﻿namespace ShoppingApp.Domain.Orders;
+
+public class Order : Entity
+{
+    public string ClientId { get; private set; }
+    public List<Product> Products { get; private set; }
+    public decimal TotalPrice { get; private set; }
+    public string DeliveryAddress { get; private set; }
+
+    private Order()
+    {
+    }
+
+    public Order (string clientId, string clientName, List<Product> products, string deliveryAddress)
+    {
+        ClientId = clientId;
+        Products = products;
+        DeliveryAddress = deliveryAddress;
+        CreatedBy = clientName;
+        EditedBy = clientName;
+        CreatedOn = DateTime.UtcNow;
+        EditedOn = DateTime.UtcNow;
+
+        TotalPrice = 0;
+        foreach (var item in Products)
+        {
+            TotalPrice += item.Price;
+        }
+
+        Validate();
+    }
+
+    private void Validate()
+    {
+        var contract = new Contract<Order>()
+            .IsNotNull(ClientId, "Client")
+            .IsNotNull(Products, "Products");
+
+        AddNotifications(contract);
+    }
+}
